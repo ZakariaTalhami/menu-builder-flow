4fc57179fe6dddf2d86ac67b254a497fbd7c497c
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2023-08-26 18:05:52 +0300

    chore: format theme file

--- src/theme.ts
+++ src/theme.ts
@@ -1,40 +1,39 @@
 export const lightTheme = {
-    bg: '#fff',
-    primary: '#ff0072',
-  
-    node: {
-      bg: '#f2f2f5',
-      color: '#222',
-      border: '#222',
-    },
+  bg: "#fff",
+  primary: "#ff0072",
 
-    nodeHandle: {
-      color: "#f2f2f5",
-      hoverColor: "#c2c2c2",
-      borderColor: "#222",
-    },
+  node: {
+    bg: "#f2f2f5",
+    color: "#222",
+    border: "#222",
+  },
 
-    minimapMaskBg: '#f2f2f5',
-  
-    controlsBg: '#fefefe',
-    controlsBgHover: '#eee',
-    controlsColor: '#222',
-    controlsBorder: '#ddd',
-  };
-  
-  export const darkTheme = {
-    bg: '#000',
-    primary: '#ff0072',
-  
-    nodeBg: '#343435',
-    nodeColor: '#f9f9f9',
-    nodeBorder: '#888',
-  
-    minimapMaskBg: '#343435',
-  
-    controlsBg: '#555',
-    controlsBgHover: '#676768',
-    controlsColor: '#dddddd',
-    controlsBorder: '#676768',
-  };
-  
+  nodeHandle: {
+    color: "#f2f2f5",
+    hoverColor: "#c2c2c2",
+    borderColor: "#222",
+  },
+
+  minimapMaskBg: "#f2f2f5",
+
+  controlsBg: "#fefefe",
+  controlsBgHover: "#eee",
+  controlsColor: "#222",
+  controlsBorder: "#ddd",
+};
+
+export const darkTheme = {
+  bg: "#000",
+  primary: "#ff0072",
+
+  nodeBg: "#343435",
+  nodeColor: "#f9f9f9",
+  nodeBorder: "#888",
+
+  minimapMaskBg: "#343435",
+
+  controlsBg: "#555",
+  controlsBgHover: "#676768",
+  controlsColor: "#dddddd",
+  controlsBorder: "#676768",
+};
